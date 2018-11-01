7c43f2f04a54647a0ebb3fbeecff077f7377b44b
Andy Scherzinger <info@andy-scherzinger.de>
2018-11-01 10:52:41 +0100

    replace further png resources with xml drawables

--- src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
@@ -912,7 +912,7 @@ public abstract class DrawerActivity extends ToolbarActivity implements DisplayU
                         }
                     };
 
-                    DisplayUtils.downloadIcon(this, firstQuota.iconUrl, target, R.drawable.ic_link_grey, size, size);
+                    DisplayUtils.downloadIcon(this, firstQuota.iconUrl, target, R.drawable.ic_link, size, size);
 
                 } else {
                     mQuotaTextLink.setVisibility(View.GONE);
@@ -1057,7 +1057,7 @@ public abstract class DrawerActivity extends ToolbarActivity implements DisplayU
                     }
                 };
 
-                DisplayUtils.downloadIcon(this, link.iconUrl, target, R.drawable.ic_link_grey, size, size);
+                DisplayUtils.downloadIcon(this, link.iconUrl, target, R.drawable.ic_link, size, size);
             }
 
             setDrawerMenuItemChecked(mCheckedMenuItem);
@@ -1071,7 +1071,7 @@ public abstract class DrawerActivity extends ToolbarActivity implements DisplayU
             if (drawable != null) {
                 menuItem.setIcon(ThemeUtils.tintDrawable(drawable, greyColor));
             } else {
-                menuItem.setIcon(R.drawable.ic_link_grey);
+                menuItem.setIcon(R.drawable.ic_link);
             }
         }
     }
--- src/main/java/com/owncloud/android/ui/activity/WhatsNewActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/WhatsNewActivity.java
@@ -5,17 +5,17 @@
  * Copyright (C) 2015 Bartosz Przybylski
  * Copyright (C) 2015 ownCloud Inc.
  * Copyright (C) 2016 Nextcloud.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -126,7 +126,7 @@ public class WhatsNewActivity extends FragmentActivity implements ViewPager.OnPa
 
     private void updateNextButtonIfNeeded() {
         if (!mProgress.hasNextStep()) {
-            mForwardFinishButton.setImageResource(R.drawable.ic_done_white);
+            mForwardFinishButton.setImageResource(R.drawable.ic_ok);
             mSkipButton.setVisibility(View.INVISIBLE);
         } else {
             mForwardFinishButton.setImageResource(R.drawable.arrow_right);
